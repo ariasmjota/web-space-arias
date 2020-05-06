e01cf13872d21b7d638d7378ea5d1b720a5eaa38
Maria Arias <majobel10@hotmail.com>
2020-05-06 13:12:18 -0500

    Updates

---
+++ precache-manifest.0a2b894d39c0e1c5a01be7f3b2b3fbd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b0d9457ea93a55907e74e0792954080",
+    "url": "/web-space-arias/index.html"
+  },
+  {
+    "revision": "8172f133c496c5c2be89",
+    "url": "/web-space-arias/static/css/main.5188b02f.chunk.css"
+  },
+  {
+    "revision": "9177a66b5ebb6acdc85f",
+    "url": "/web-space-arias/static/js/2.48536cd2.chunk.js"
+  },
+  {
+    "revision": "1b730b11501651dc0ff37e34a614c377",
+    "url": "/web-space-arias/static/js/2.48536cd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8172f133c496c5c2be89",
+    "url": "/web-space-arias/static/js/main.6420748f.chunk.js"
+  },
+  {
+    "revision": "4aa9026a3265b0a22e96",
+    "url": "/web-space-arias/static/js/runtime-main.6c9ef09c.js"
+  }
+]);
